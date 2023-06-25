331ac5cf6439fdad4b904e13251ad6ad844e6369
蒲若坤 <puruokun@bizseer.com>
2023-06-25 13:26:41 +0000

    feat: make chat history with detail trace togglable

--- web.py
+++ web.py
@@ -5,9 +5,11 @@ import threading
 import gradio as gr
 from uuid import uuid4
 from pathlib import Path
-from typing import List, Dict, Tuple, Type, Optional, Callable
+from typing import List, Dict, Tuple, Type, Optional, Callable, Any
+from collections import namedtuple
 
 import gradio as gr
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools
@@ -18,10 +20,13 @@ from langchain.chains.base import Chain
 from langchain.base_language import BaseLanguageModel
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from dotenv import load_dotenv
+from langchain.schema import AgentAction, AgentFinish, LLMResult
+
 from utils.fetch_login_token import fetch_login_token
 
 sys.path.append(str(Path(__file__).parent.joinpath("opsgpt-prompt")))
+
+ChatPair = namedtuple("ChatPair", ["soliloquy", "human", "bot"])
 
 from opsgpt import (
     MetacubeToolkit,
@@ -91,10 +96,72 @@ class CustomizedCallbackHandler(StreamingStdOutCallbackHandler):
     def on_llm_start(
         self, serialized, prompts, **kwargs
     ) -> None:
-        self.out.put(['new_msg'])
+        self.out.put(['new_display'])
 
     def on_llm_new_token(self, token, **kwargs) -> None:
         self.out.put(['delta', token])
+    
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        self.out.put(['pop'])
+
+    def on_chain_start(
+        self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs: Any
+    ) -> None:
+        """Print out that we are entering a chain."""
+        self.out.put(['new_hidden'])
+
+        class_name = serialized.get("id", "")
+        class_name = class_name[-1] if class_name else serialized.get("name", "")
+        msg = f"> Entering new {class_name} chain...\n"
+        self.out.put(['delta', msg])
+
+    def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
+        """Print out that we finished a chain."""
+        self.out.put(['new_hidden'])
+
+        msg = "> Finished chain.\n"
+        self.out.put(['delta', msg])
+    
+    def on_agent_action(
+        self, action: AgentAction, color: Optional[str] = None, **kwargs: Any
+    ) -> Any:
+        """Run on agent action."""
+        self.out.put(['delta', action.log])
+    
+    def on_tool_end(
+        self,
+        output: str,
+        color: Optional[str] = None,
+        observation_prefix: Optional[str] = None,
+        llm_prefix: Optional[str] = None,
+        **kwargs: Any,
+    ) -> None:
+        """If not the final action, print out observation."""
+        # if observation_prefix is not None:
+        #     self.out.put(['delta', f"\n{observation_prefix}"])
+        self.out.put(['delta', output])
+        # if llm_prefix is not None:
+        #     self.out.put(['delta', f"\n{llm_prefix}"])
+
+    def on_text(
+        self,
+        text: str,
+        color: Optional[str] = None,
+        end: str = "",
+        **kwargs: Any,
+    ) -> None:
+        """Run when agent ends."""
+        self.out.put(['delta', text])
+        self.out.put(['delta', end])
+
+    def on_agent_finish(
+        self, finish: AgentFinish, color: Optional[str] = None, **kwargs: Any
+    ) -> None:
+        """Run on agent end."""
+        self.out.put(['delta', finish.log + "\n"])
+        self.out.put(['new_display'])
+        self.out.put(['delta', finish.return_values["output"]])
+
 
 class Task(threading.Thread):
     agent: Chain
@@ -108,7 +175,8 @@ class Task(threading.Thread):
         self.msg = msg
 
     def run(self):
-        self.agent.run(self.msg)
+        callback = CustomizedCallbackHandler(self.reply)
+        self.agent.run(self.msg, callbacks=[callback])
         self.reply.put(['end'])
         
 class TaskController:
@@ -136,14 +204,12 @@ class TaskController:
         
         self._mode = name
 
-        callback = CustomizedCallbackHandler(self.reply)
-
         self._msgbuf = queue.Queue()
         def input_func():
             self.reply.put(['end'])
             return self._msgbuf.get()
         
-        self._agent = create_bizseer_agents(name, callback, input_func)
+        self._agent = create_bizseer_agents(name, input_func)
 
     def launch(self, msg: str):
         self._task = Task(self._agent, self.reply, msg)
@@ -163,9 +229,16 @@ def get_task_controller(session_id: str) -> TaskController:
         task_controller_map[session_id] = TaskController()    
     return task_controller_map[session_id]
 
-def get_answer(query: str, history: List[List[str]], mode: str, session_id: str):
-    history.append([query, None])
-    yield history, ""
+def get_answer(query: str, full_history: List[ChatPair], mode: str, session_id: str, display_soliloquy: bool) -> Tuple[
+    List[List[str]],
+    str,
+    List[ChatPair],
+]:
+    full_history.append(ChatPair(soliloquy=False, human=query, bot=None))
+    history = [[record.human, record.bot] for record in full_history if not record.soliloquy or display_soliloquy]
+
+    yield history, "", full_history
+
     taskctl = get_task_controller(session_id)
     taskctl.ensure_mode(mode)
     if taskctl.is_idle():
@@ -175,21 +248,38 @@ def get_answer(query: str, history: List[List[str]], mode: str, session_id: str)
 
     while True:
         cmd, *args = taskctl.reply.get()
-        if cmd == 'new_msg':
-            if history[-1][-1] is not None:
-                history.append([None, None])
+        if cmd == 'new_display':
+            full_history.append(ChatPair(soliloquy=False, human=None, bot=""))
+            history.append([None, ""])
+        elif cmd == 'new_hidden':
+            full_history.append(ChatPair(soliloquy=True, human=None, bot=""))
+            if display_soliloquy:
+                history.append([None, ""])
         elif cmd == 'delta':
-            if history[-1][-1] is None:
-                history[-1][-1] = ''
-            history[-1][-1] += args[0]
-            yield history, ""
+            last_record = full_history.pop()
+            full_history.append(
+                ChatPair(
+                    soliloquy=last_record.soliloquy,
+                    human=last_record.human,
+                    bot=last_record.bot + args[0]
+                )
+            )
+
+            if not last_record.soliloquy or display_soliloquy:
+                history[-1][-1] += args[0]
+
+            yield history, "", full_history
         elif cmd == 'end':
             break
-    return history, ""
+        elif cmd == 'pop':
+            full_history.pop()
+            history.pop()
+            yield history, "", full_history
+    return history, "", full_history
 
 
-def create_human_as_tools(callback: BaseCallbackHandler, input_func: Callable) -> List[BaseTool]:
-    llm = ChatOpenAI(model_name='gpt-4-0613', streaming=True, callbacks=[callback], temperature=0)
+def create_human_as_tools(input_func: Callable) -> List[BaseTool]:
+    llm = ChatOpenAI(model_name='gpt-4-0613', streaming=True, temperature=0)
     return load_tools(
         ["human"], 
         llm=llm,
@@ -197,8 +287,8 @@ def create_human_as_tools(callback: BaseCallbackHandler, input_func: Callable) -
     )
 
 
-def create_bizseer_agents(toolkit_name: str, callback: BaseCallbackHandler, input_func: Callable) -> Chain:
-    llm = OpenAI(model_name="text-davinci-003", streaming=True, callbacks=[callback], max_tokens=-1)
+def create_bizseer_agents(toolkit_name: str, input_func: Callable) -> Chain:
+    llm = OpenAI(model_name="text-davinci-003", streaming=True, max_tokens=-1)
 
     auth_token = fetch_login_token()
     requests = Requests(headers={"Authorization": auth_token})
@@ -210,12 +300,11 @@ def create_bizseer_agents(toolkit_name: str, callback: BaseCallbackHandler, inpu
             clz.from_llm(llm, requests=requests).get_tools()
         )
     
-    tools.extend(create_human_as_tools(callback=callback, input_func=input_func))
+    tools.extend(create_human_as_tools(input_func=input_func))
     
     return OpsGPTAgent.from_llm_and_tools(
         llm=llm,
-        tools=tools,
-        verbose=True
+        tools=tools
     )
 
 def load_agent(session_id: str, mode: str):
@@ -229,11 +318,19 @@ def load_agent(session_id: str, mode: str):
         [simple_name(tool.name), tool.description.strip()] for tool in taskctl._agent.tools
     ]
 
+def toggle_trace(full_history: List[ChatPair], display_soliloquy: bool) -> Tuple[
+    List[List[str]],
+    bool
+]:
+    history = [[record.human, record.bot] for record in full_history if not record.soliloquy or display_soliloquy]
+    
+    return [history, display_soliloquy]
 
 with gr.Blocks(css=CUSTOMIZED_CSS) as demo:
     gr.Markdown(WEBUI_TITLE)
 
     session_id = gr.State(lambda: str(uuid4()))
+    full_history = gr.State(lambda: [])
 
     with gr.Tab("对话"):
         with gr.Row():
@@ -254,12 +351,21 @@ with gr.Blocks(css=CUSTOMIZED_CSS) as demo:
                                    value=AVAILABLE_MODES[0],
                                    multiselect=False)
                 clear_btn = gr.Button(value="清空对话历史")
+                display_trace = gr.Checkbox(value=False, label="是否显示详细推理过程?")
 
-            query.submit(get_answer,
-                        [query, chatbot, mode, session_id],
-                        [chatbot, query])
+            query.submit(
+                get_answer,
+                [query, full_history, mode, session_id, display_trace],
+                [chatbot, query, full_history]
+            )
             
-            clear_btn.click(lambda: [], [], [chatbot])
+            display_trace.change(
+                toggle_trace,
+                [full_history, display_trace],
+                [chatbot, display_trace]
+            )
+            
+            clear_btn.click(lambda: [[], []], [], [chatbot, full_history])
             mode.change(load_agent, inputs=[session_id, mode], outputs=[table])
 
     demo.load(load_agent, inputs=[session_id, mode], outputs=[table])
